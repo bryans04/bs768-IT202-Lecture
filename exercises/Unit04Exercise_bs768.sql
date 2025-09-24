2656288b4fc6482f561a1be4129f36caa9f2b048
Bryan Sanchez <bryansanchez6191@outlook.com>
2025-09-24 10:21:54 -0400

    Unit 4 Exercise

---
+++ exercises/Unit04Exercise_bs768.sql
@@ -0,0 +1,67 @@
+CREATE TABLE golfers
+
+  (golferid int primary key,
+
+  name varchar(100),
+
+  address varchar(200),
+
+  phone varchar(20));
+
+  INSERT INTO golfers VALUES
+
+  (100, 'Rich', '123 Main St.', '555-1234');
+
+  INSERT INTO golfers VALUES
+
+  (101, 'Barbara', '123 Main St.', '555-5678');
+
+  INSERT INTO golfers VALUES
+
+  (102, 'Susan', '456 Oak St.', '555-8765');
+
+    INSERT INTO golfers VALUES
+    
+    (103, 'James', '789 Pine St.', '555-4321');
+
+    SELECT * FROM golfers
+
+    CREATE TABLE games
+
+  (gameid int auto_increment primary key,
+
+  golferid int,
+
+  score int);
+
+  INSERT INTO games (golferid, score) VALUES (100, 110);
+
+  INSERT INTO games (golferid, score) VALUES (100, 115);
+
+  INSERT INTO games (golferid, score) VALUES (100, 105);
+
+  INSERT INTO games (golferid, score) VALUES (101, 110);
+
+  INSERT INTO games (golferid, score) VALUES (101, 112);
+
+  INSERT INTO games (golferid, score) VALUES (101, 130);
+
+  INSERT INTO games (golferid, score) VALUES (102, 120);
+
+  INSERT INTO games (golferid, score) VALUES (102, 125);
+  
+  INSERT INTO games (golferid, score) VALUES (102, 140);
+
+  INSERT INTO games (golferid, score) VALUES (103, 130);
+
+  INSERT INTO games (golferid, score) VALUES (103, 135);
+
+  INSERT INTO games (golferid, score) VALUES (103, 128);
+
+SELECT * FROM games;
+
+SELECT golferid, name FROM golfers ORDER BY name
+
+SELECT COUNT(score) AS games, AVG(score) AS average FROM games WHERE golferid = 102
+
+SELECT COUNT(score) AS games, AVG(score) AS average FROM games WHERE golferid = 103
